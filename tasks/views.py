f36e032314405db13789bb3862a73b7c9400bfa7
James Alhassan <jamesalhassan052@gmail.com>
2023-03-05 03:57:22 +0000

    Add models

--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Task
 
 # Create your views here.
 def index(request):
+    task = Task.objects.all()
+    context = {'task': task}
     return HttpResponse('Hello World')
